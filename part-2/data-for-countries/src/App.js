afdd7461fb9c42cd427df387e14c9b0b7e3d10bd
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-11-30 09:23:39 +0000

    2.14*: Data for countries, step3

--- part-2/data-for-countries/src/App.js
+++ part-2/data-for-countries/src/App.js
@@ -4,7 +4,9 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [countrySearch, setCountrySearch] = useState("");
   const [selected, setSelected] = useState();
-
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  const [weather, setWeather] = useState("");
+  console.log(API_KEY)
   function handleCountrySearch(e) {
     setCountrySearch(e.target.value);
   }
@@ -16,8 +18,16 @@ function App() {
       )
       .then((response) => {
         setCountries(response.data);
-      }, []);
-  });
+      });
+    if (countries.length === 1)
+      axios
+        .get(
+          `http://api.weatherstack.com/current?access_key=${API_KEY}&query=${countries[0].capital}&units=m`
+        )
+        .then((response) => {
+          setWeather(response.data);
+        });
+  }, [countrySearch]);
   return (
     <div className="App">
       Find countries
@@ -25,20 +35,20 @@ function App() {
       <br />
       {countries.lenght === 0 ? (
         <p>Search a country</p>
-      ) : countries.length === 1 ? (
-        <Detail country={countries[0]} />
+      ) : countries.length === 1 && weather != "" ? (
+        <Details country={countries[0]} weather={weather} />
       ) : countries.length > 10 ? (
         <p>Too many</p>
       ) : (
         countries.map((country, index) => {
           if (index === selected) {
-            return <Detail country = {country} />;
+            return <Details country={country} weather={weather} />;
           } else {
             return (
               <div>
                 <p key={country.name}>
                   {country.name}
-                  
+
                   <button
                     onClick={() => {
                       setSelected(index);
@@ -56,7 +66,17 @@ function App() {
   );
 }
 
-const Detail = ({ country }) => {
+const Details = ({ country, weather }) => {
+  console.log(weather);
+  return (
+    <div>
+      <Info country = {country} />
+      <Weather country = {country} weather = {weather} />
+    </div>
+  );
+};
+
+function Info({country}) {
   return (
     <div>
       <h1>{country.name}</h1>
@@ -74,6 +94,20 @@ const Detail = ({ country }) => {
       <img style={{ maxWidth: 250 }} src={country.flag} />
     </div>
   );
-};
+}
+
+function Weather({weather,country}) {
+  return (
+    <div>
+      <h2>Weather in {country.capital} </h2>
+      <p>Temperature: {weather.current.temperature} Celsius </p>
+      <img src={weather.current.weather_icons} />
+      <p>
+        Wind:{weather.current.wind_speed} mph direction{" "}
+        {weather.current.wind_dir}{" "}
+      </p>
+    </div>
+  );
+}
 
 export default App;
