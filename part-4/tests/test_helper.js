e9e9eb8540613813b98a343423ab5cb087a1b4a9
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-14 14:57:01 +0000

    refactor test

---
+++ part-4/tests/test_helper.js
@@ -0,0 +1,112 @@
+const blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: "Hello World",
+    likes: 500,
+    author: "Gregory Ward",
+    url: "https://www.example.com/",
+    id: "zAgXiPp9OvtBjJwsUvIVaj24",
+  },
+
+  {
+    title: "Hello Universe",
+    likes: 250,
+    author: "Oliwia Coombes",
+    url: "https://www.example.net",
+    id: "Yujef1ugEkyoeL6Mg3j7akel",
+  },
+  {
+    title: "Hello Nothing",
+    likes: 350,
+    author: "Gregory Ward",
+    url: "https://www.example.org",
+    id: "5s5bW14mQZHFdcKLpUFQausb",
+  },
+];
+
+const dummy = (blogs) => {
+  return 1;
+};
+
+const blogsInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
+const totalLikes = (blogs) => {
+  const totalLikes = blogs.reduce((accumulator, blog) => {
+    return accumulator + blog.likes;
+  }, 0);
+
+  return totalLikes;
+};
+
+const mostLiked = (blogs) => {
+  if (blogs.length === 0) return null;
+  const likes = blogs.map((blog) => blog.likes);
+  const mostLikes = Math.max(...likes);
+  const mostLiked = blogs.find((blog) => blog.likes === mostLikes);
+  const blog = {
+    title: mostLiked.title,
+  };
+  return mostLiked;
+};
+
+const mostCommon = (array) => {
+  let mf = 1;
+  let m = 0;
+  let item = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    for (let j = i; j < array.length; j++) {
+      if (array[i] == array[j]) m++;
+      if (mf < m) {
+        mf = m;
+        item = array[i];
+      }
+    }
+    m = 0;
+  }
+  return { item, mf };
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return null;
+  const authors = blogs.map((blog) => blog.author);
+  //get most repeated author
+  const mostBlogs = mostCommon(authors);
+  return { author: mostBlogs.item, blogs: mostBlogs.mf };
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return null;
+  if (blogs.length === 1)
+    return { author: blogs[0].author, likes: blogs[0].likes };
+  const sortArray = blogs.sort((a, b) => (a.author > b.author ? 1 : -1));
+  let sum = sortArray[0].likes;
+  let highestLikes = 0;
+  let highestAuthor;
+  for (let i = 0; i < sortArray.length - 1; i++) {
+    if (sortArray[i].author === sortArray[i + 1].author) {
+      sum += sortArray[i + 1].likes;
+    } else {
+      sum = sortArray[i + 1].likes;
+    }
+    if (sum > highestLikes) {
+      highestLikes = sum;
+      highestAuthor = sortArray[i + 1].author;
+      console.log(highestLikes);
+    }
+  }
+  return { author: highestAuthor, likes: highestLikes };
+};
+
+module.exports = {
+  dummy,
+  initialBlogs,
+  blogsInDb,
+  totalLikes,
+  mostLiked,
+  mostBlogs,
+  mostLikes,
+};
