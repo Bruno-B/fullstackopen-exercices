888b724985cb7f6e9bab4b57af328df17e5bce1a
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-14 10:54:03 +0000

    4.5*: helper functions and unit tests, step3

--- part-4/tests/blog_api.test.js
+++ part-4/tests/blog_api.test.js
@@ -9,16 +9,25 @@ test("dummy test", () => {
 
 const blogOne = {
     title:"Hello World",
-    likes:500
+    likes:500,
+    author:"Jamal Nguyen",
+    url:"https://www.example.com/",
+    id:"zAgXiPp9OvtBjJwsUvIVaj24"
 }
 
 const blogTwo = {
     title:"Hello Universe",
-    likes:250
+    likes:250,
+    author:"Oliwia Coombes",
+    url:"https://www.example.net",
+    id:"Yujef1ugEkyoeL6Mg3j7akel"
 }
 const blogThree = {
     title:"Hello Nothing",
-    likes:250
+    likes:250,
+    author:"Gregory Ward",
+    url:"https://www.example.org",
+    id:"5s5bW14mQZHFdcKLpUFQausb"
 }
 
 describe("total likes", () => {
@@ -45,17 +54,17 @@ describe("favourite blog",()=>{
     test("of empty list is zero",()=>{
         const blogs = []
         const result = helper.mostLiked(blogs)
-        expect(result).toBe(null)
+        expect(result).toEqual(null)
     })
     test("when list has only one blog returns that blog",()=>{
         const blogs = [blogOne]
         const result = helper.mostLiked(blogs)
-        expect(result).toBe(blogOne)
+        expect(result).toEqual(blogOne)
     })
 
     test("of a bigger list return most liked",()=>{
         const blogs = [blogOne,blogTwo,blogThree]
         const result = helper.mostLiked(blogs)
-        expect(result).toBe(blogOne)
+        expect(result).toEqual(blogOne)
     })
 })
