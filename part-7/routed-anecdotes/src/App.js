1c03618e8de65e163c4ba2829dc888178de87be8
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-27 11:51:17 +0000

    7.4: anecdotes and hooks step1

--- part-7/routed-anecdotes/src/App.js
+++ part-7/routed-anecdotes/src/App.js
@@ -7,6 +7,7 @@ import {
   useHistory,
   useLocation,
 } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
@@ -91,9 +92,9 @@ const Notification = ({ message }) => {
 };
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
   const history = useHistory();
 
   const handleSubmit = (e) => {
@@ -117,27 +118,15 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
-          />
+          <input {...content} />
         </div>
         <div>
           author
-          <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
-          />
+          <input {...author} />
         </div>
         <div>
           url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+          <input {...info} />
         </div>
         <button>create</button>
       </form>
@@ -206,7 +195,7 @@ const App = () => {
       <Menu />
       <Switch>
         <Route exact path="/">
-          {location.state && <Notification message = {location.state.message} />}
+          {location.state && <Notification message={location.state.message} />}
           <AnecdoteList anecdotes={anecdotes} />
         </Route>
         <Route path="/anecdotes/:id">
