d2d7e08fbbad7d04e06d69a7760a04aa9edad6d0
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-28 23:46:20 +0000

    5.9*: Blog list frontend, step9

--- part-5/bloglist-frontend/src/App.js
+++ part-5/bloglist-frontend/src/App.js
@@ -130,9 +130,11 @@ const App = () => {
             blogService={blogService}
           />
         </Togglable>
-        {blogs.map((blog) => (
-          <Blog key={blog.id} blog={blog} />
-        ))}
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
       </div>
     );
   };
