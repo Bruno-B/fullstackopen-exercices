e0367dfba5a7a72e58fce6ce3934c4cdb43ff22b
Bruno Baptista <bruno.baptista.dev@gmail.com>
2020-12-29 00:06:18 +0000

    5.7* Blog list frontend, step7

--- part-5/bloglist-frontend/src/components/Blog.js
+++ part-5/bloglist-frontend/src/components/Blog.js
@@ -1,10 +1,35 @@
-import React from "react";
-const Blog = ({ blog }) => (
-  <div>
-    <p>
-      {blog.title} {blog.author}
-    </p>
-  </div>
-);
+import React, { useState } from "react";
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+  const [view, setView] = useState(false);
+  const handleClick = () => {
+    setView(!view);
+  };
+  return (
+    <div style={blogStyle}>
+      <p>
+        {blog.title} {blog.author}
+        {view ? (
+          <button onClick={handleClick}>hide</button>
+        ) : (
+          <button onClick={handleClick}>view</button>
+        )}
+      </p>
+      {view && (
+        <div>
+          <p>{blog.url}</p>
+          <p>likes {blog.likes} <button>like</button> </p>
+          <p>{blog.author} </p>
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default Blog;
