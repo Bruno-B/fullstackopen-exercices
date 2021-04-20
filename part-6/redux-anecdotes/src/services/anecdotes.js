4c31d32ea818dc4261415b66d5eaa9a701997078
Bruno <bruno.baptista.dev@gmail.com>
2021-04-20 14:29:38 +0100

    6.13 Anecdotes and the backend, step1

---
+++ part-6/redux-anecdotes/src/services/anecdotes.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:3001/anecdotes";
+
+export const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
+};
+
+const service = {
+  getAll,
+};
+
+export default service;
--- part-6/redux-anecdotes/src/store.js
+++ part-6/redux-anecdotes/src/store.js
@@ -1,8 +1,9 @@
 import { combineReducers, createStore } from "redux";
-import anecdoteReducer from "./reducers/anecdoteReducer";
+import anecdoteReducer, { createAnecdote } from "./reducers/anecdoteReducer";
 import notificationReducer from "./reducers/notificationReducer";
 import filterReducer from "./reducers/filterReducer";
 import { composeWithDevTools } from "redux-devtools-extension";
+import anecdoteService from "./services/anecdotes";
 
 const rootReducer = combineReducers({
   anecdotes: anecdoteReducer,
@@ -10,4 +11,11 @@ const rootReducer = combineReducers({
   filter: filterReducer,
 });
 const store = createStore(rootReducer, composeWithDevTools());
+
+anecdoteService.getAll().then(anecdotes => {
+  anecdotes.forEach(anecdote => {
+    store.dispatch(createAnecdote(anecdote.content));
+  });
+});
+
 export default store;
