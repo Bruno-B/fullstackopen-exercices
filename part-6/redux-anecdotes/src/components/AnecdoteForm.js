95693b054ed4b560dcf993280e7dae56491886a3
Bruno <bruno.baptista.dev@gmail.com>
2021-04-20 14:42:59 +0100

    6.14 Anecdotes and the backend, step2

--- part-6/redux-anecdotes/src/components/AnecdoteForm.js
+++ part-6/redux-anecdotes/src/components/AnecdoteForm.js
@@ -2,31 +2,31 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
 import { displayNotification } from "../reducers/notificationReducer";
+import anecdoteService from "../services/anecdotes";
 
 const AnecdoteForm = () => {
-
   const dispatch = useDispatch();
   const [newAnecdoteText, setNewAnecdoteText] = useState("");
 
-  const create = (e) => {
+  const create = async (e) => {
     e.preventDefault();
-    dispatch(createAnecdote(newAnecdoteText));
     dispatch(displayNotification(`Added ${newAnecdoteText}`));
+    const newAnecdote = await anecdoteService.createNew(newAnecdoteText);
+    dispatch(createAnecdote(newAnecdoteText));
     setNewAnecdoteText("");
   };
 
   return (
     <form onSubmit={create}>
-    <div>
-      <input
-        value={newAnecdoteText}
-        onChange={(e) => setNewAnecdoteText(e.target.value)}
-      />
-    </div>
-    <button>create</button>
-  </form>
-  )
-}
-
+      <div>
+        <input
+          value={newAnecdoteText}
+          onChange={(e) => setNewAnecdoteText(e.target.value)}
+        />
+      </div>
+      <button>create</button>
+    </form>
+  );
+};
 
 export default AnecdoteForm;
--- part-6/redux-anecdotes/src/services/anecdotes.js
+++ part-6/redux-anecdotes/src/services/anecdotes.js
@@ -2,13 +2,24 @@ import axios from "axios";
 
 const baseUrl = "http://localhost:3001/anecdotes";
 
-export const getAll = async () => {
+const getAll = async () => {
   const response = await axios.get(baseUrl);
   return response.data;
 };
 
+const createNew = async(text) => {
+  const anecdote = {
+    content:text,
+    likes:0,
+  }
+  const response = await axios.post(baseUrl,anecdote);
+  return response.data;
+}
+
+
 const service = {
   getAll,
+  createNew
 };
 
 export default service;
